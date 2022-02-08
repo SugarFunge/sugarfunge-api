6179e8406923fb82d95ad78898909ca02b8b50bf
Carlos Lopez <lopezandara.carlos@gmail.com>
2022-02-08 23:23:40 +0100

    Added validator set functions

---
+++ src/validator.rs
@@ -0,0 +1,98 @@
+use crate::state::*;
+use crate::sugarfunge;
+use crate::util::*;
+use actix_web::{error, web, HttpResponse};
+use serde::{Deserialize, Serialize};
+use serde_json::json;
+use std::str::FromStr;
+use subxt::PairSigner;
+
+#[derive(Serialize, Deserialize)]
+pub struct AddValidatorInput {
+    seed: String,
+    validator_id: String,
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct AddValidatorOutput {
+    validator_id: String,
+}
+
+pub async fn add_validator(
+    data: web::Data<AppState>,
+    req: web::Json<AddValidatorInput>,
+) -> error::Result<HttpResponse> {
+    let pair = get_pair_from_seed(&req.seed)?;
+    let signer = PairSigner::new(pair);
+    let validator_id = sp_core::sr25519::Public::from_str(&req.validator_id).map_err(map_account_err)?;
+    let validator_id = sp_core::crypto::AccountId32::from(validator_id);
+    let api = data.api.lock().unwrap();
+    let result = api
+        .tx()
+        .validator_set()
+        .add_validator(validator_id)
+        .sign_and_submit_then_watch(&signer)
+        .await
+        .map_err(map_subxt_err)?
+        .wait_for_finalized_success()
+        .await
+        .map_err(map_subxt_err)?;
+
+    let result = result
+        .find_first_event::<sugarfunge::validator_set::events::ValidatorAdditionInitiated>()
+        .map_err(map_subxt_err)?;
+
+    match result {
+        Some(event) => Ok(HttpResponse::Ok().json(AddValidatorOutput {
+            validator_id: event.0.to_string(),
+        })),
+        None => Ok(HttpResponse::BadRequest().json(RequestError {
+            message: json!("Failed to find sugarfunge::validator::events::AddValidator"),
+        })),
+    }
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct RemoveValidatorInput {
+    seed: String,
+    validator_id: String,
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct RemoveValidatorOutput {
+    validator_id: String,
+}
+
+pub async fn remove_validator(
+    data: web::Data<AppState>,
+    req: web::Json<RemoveValidatorInput>,
+) -> error::Result<HttpResponse> {
+    let pair = get_pair_from_seed(&req.seed)?;
+    let signer = PairSigner::new(pair);
+    let validator_id = sp_core::sr25519::Public::from_str(&req.validator_id).map_err(map_account_err)?;
+    let validator_id = sp_core::crypto::AccountId32::from(validator_id);
+    let api = data.api.lock().unwrap();
+    let result = api
+        .tx()
+        .validator_set()
+        .remove_validator(validator_id)
+        .sign_and_submit_then_watch(&signer)
+        .await
+        .map_err(map_subxt_err)?
+        .wait_for_finalized_success()
+        .await
+        .map_err(map_subxt_err)?;
+
+    let result = result
+        .find_first_event::<sugarfunge::validator_set::events::ValidatorRemovalInitiated>()
+        .map_err(map_subxt_err)?;
+
+    match result {
+        Some(event) => Ok(HttpResponse::Ok().json(RemoveValidatorOutput {
+            validator_id: event.0.to_string(),
+        })),
+        None => Ok(HttpResponse::BadRequest().json(RequestError {
+            message: json!("Failed to find sugarfunge::validator::events::RemoveValidator"),
+        })),
+    }
+}
