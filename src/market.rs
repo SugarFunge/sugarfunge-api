df66e8d072f7a9162cbb2fca163de51336f04f9a
Alex Rozgo <alex.rozgo@gmail.com>
2022-02-22 02:44:32 -0600

    provide market types conversions

--- src/market.rs
+++ src/market.rs
@@ -1,14 +1,15 @@
-use std::collections::BTreeMap;
-
 use crate::state::*;
 use crate::sugarfunge;
+use crate::sugarfunge::runtime_types::frame_support::storage::bounded_vec::BoundedVec;
+use crate::sugarfunge::runtime_types::sugarfunge_market;
 use crate::util::*;
 use actix_web::{error, web, HttpResponse};
 use serde::{Deserialize, Serialize};
 use serde_json::json;
+use std::str::FromStr;
 use subxt::PairSigner;
 
-#[derive(Serialize, Deserialize)]
+#[derive(Serialize, Deserialize, Clone)]
 pub enum AmountOp {
     Equal,
     LessThan,
@@ -17,7 +18,7 @@ pub enum AmountOp {
     GreaterEqualThan,
 }
 
-#[derive(Serialize, Deserialize)]
+#[derive(Serialize, Deserialize, Clone)]
 pub enum RateAction {
     Transfer,
     Mint,
@@ -25,14 +26,14 @@ pub enum RateAction {
     Has(AmountOp),
 }
 
-#[derive(Serialize, Deserialize)]
+#[derive(Serialize, Deserialize, Clone)]
 pub enum RateAccount {
     Market,
     Account(String),
     Buyer,
 }
 
-#[derive(Serialize, Deserialize)]
+#[derive(Serialize, Deserialize, Clone)]
 pub struct AssetRate {
     class_id: u64,
     asset_id: u64,
@@ -42,8 +43,74 @@ pub struct AssetRate {
     to: RateAccount,
 }
 
+impl Into<sugarfunge_market::AmountOp> for AmountOp {
+    fn into(self) -> sugarfunge_market::AmountOp {
+        match self {
+            AmountOp::Equal => sugarfunge_market::AmountOp::Equal,
+            AmountOp::GreaterEqualThan => sugarfunge_market::AmountOp::GreaterEqualThan,
+            AmountOp::GreaterThan => sugarfunge_market::AmountOp::GreaterThan,
+            AmountOp::LessEqualThan => sugarfunge_market::AmountOp::LessEqualThan,
+            AmountOp::LessThan => sugarfunge_market::AmountOp::LessThan,
+        }
+    }
+}
+
+impl Into<sugarfunge_market::RateAccount<subxt::sp_runtime::AccountId32>> for RateAccount {
+    fn into(self) -> sugarfunge_market::RateAccount<subxt::sp_runtime::AccountId32> {
+        match self {
+            RateAccount::Buyer => sugarfunge_market::RateAccount::Buyer,
+            RateAccount::Market => sugarfunge_market::RateAccount::Market,
+            RateAccount::Account(account) => {
+                let account = sp_core::sr25519::Public::from_str(&account).unwrap();
+                let account = sp_core::crypto::AccountId32::from(account);
+                sugarfunge_market::RateAccount::Account(account)
+            }
+        }
+    }
+}
+
+impl Into<sugarfunge_market::RateAction> for RateAction {
+    fn into(self) -> sugarfunge_market::RateAction {
+        match self {
+            RateAction::Transfer => sugarfunge_market::RateAction::Transfer,
+            RateAction::Mint => sugarfunge_market::RateAction::Mint,
+            RateAction::Burn => sugarfunge_market::RateAction::Burn,
+            RateAction::Has(op) => sugarfunge_market::RateAction::Has(op.into()),
+        }
+    }
+}
+
+impl Into<sugarfunge_market::AssetRate<subxt::sp_runtime::AccountId32, u64, u64>> for AssetRate {
+    fn into(self) -> sugarfunge_market::AssetRate<subxt::sp_runtime::AccountId32, u64, u64> {
+        sugarfunge_market::AssetRate::<subxt::sp_runtime::AccountId32, u64, u64> {
+            class_id: self.class_id,
+            asset_id: self.asset_id,
+            action: self.action.into(),
+            amount: self.amount as i128,
+            from: self.from.into(),
+            to: self.to.into(),
+            __subxt_unused_type_params: Default::default(),
+        }
+    }
+}
+
+fn extrinsinc_rates(
+    in_rates: &Vec<AssetRate>,
+) -> BoundedVec<sugarfunge_market::AssetRate<subxt::sp_runtime::AccountId32, u64, u64>> {
+    BoundedVec(
+        in_rates
+            .iter()
+            .map(|rate| {
+                <AssetRate as Into<
+                    sugarfunge_market::AssetRate<subxt::sp_runtime::AccountId32, u64, u64>,
+                >>::into(rate.clone())
+            })
+            .collect(),
+    )
+}
+
 #[derive(Serialize, Deserialize)]
-pub struct MarketRate {
+pub struct Rates {
     rates: Vec<AssetRate>,
     metadata: Vec<u8>,
 }
@@ -96,7 +163,7 @@ pub struct CreateMarketRateInput {
     seed: String,
     market_id: u64,
     market_rate_id: u64,
-    market_rate: MarketRate,
+    rates: Rates,
 }
 #[derive(Serialize, Deserialize)]
 pub struct CreateMarketRateOutput {
@@ -112,10 +179,13 @@ pub async fn create_market_rate(
     let pair = get_pair_from_seed(&req.seed)?;
     let signer = PairSigner::new(pair);
     let api = data.api.lock().unwrap();
+
+    let rates = extrinsinc_rates(&req.rates.rates);
+
     let result = api
         .tx()
         .market()
-        .create_market_rate(req.market_id, req.market_rate_id, req.market_rate)
+        .create_market_rate(req.market_id, req.market_rate_id, rates)
         .sign_and_submit_then_watch(&signer)
         .await
         .map_err(map_subxt_err)?
@@ -151,7 +221,7 @@ pub struct DepositAssetsOutput {
     market_id: u64,
     market_rate_id: u64,
     amount: u128,
-    balances: BTreeMap<AssetRate, i128>,
+    // balances: BTreeMap<AssetRate, i128>,
 }
 
 pub async fn deposit_assets(
@@ -180,7 +250,7 @@ pub async fn deposit_assets(
             market_id: event.market_id,
             market_rate_id: event.market_rate_id,
             amount: event.amount,
-            balances: event.balances,
+            // balances: event.balances,
         })),
         None => Ok(HttpResponse::BadRequest().json(RequestError {
             message: json!("Failed to find sugarfunge::market::events::Deposit"),
@@ -202,7 +272,7 @@ pub struct ExchangeAssetsOutput {
     market_id: u64,
     market_rate_id: u64,
     amount: u128,
-    balances: BTreeMap<AssetRate, i128>,
+    // balances: BTreeMap<AssetRate, i128>,
 }
 
 pub async fn exchange_assets(
@@ -231,7 +301,7 @@ pub async fn exchange_assets(
             market_id: event.market_id,
             market_rate_id: event.market_rate_id,
             amount: event.amount,
-            balances: event.balances,
+            // balances: event.balances,
         })),
         None => Ok(HttpResponse::BadRequest().json(RequestError {
             message: json!("Failed to find sugarfunge::market::events::Exchange"),
