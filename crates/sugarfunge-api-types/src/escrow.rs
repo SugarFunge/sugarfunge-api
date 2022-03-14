6f97220c8644029b2323be0d4298d403a0d77b6d
Carlos Augusto Lopez Andara <68576818+carloslopezandara@users.noreply.github.com>
2022-03-14 20:54:03 +0100

    Modified the escrow functions

--- crates/sugarfunge-api-types/src/escrow.rs
+++ crates/sugarfunge-api-types/src/escrow.rs
@@ -1,43 +1,59 @@
 use serde::{Deserialize, Serialize};
 
 #[derive(Serialize, Deserialize)]
+pub struct RegisterEscrowInput {
+    pub seed: String,
+    pub class_id: u64,
+    pub metadata: serde_json::Value,
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct RegisterEscrowOutput {
+    pub who: String,
+    pub class_id: u64,
+}
+
+#[derive(Serialize, Deserialize)]
 pub struct CreateEscrowInput {
     pub seed: String,
-    pub owner: String,
+    pub class_id: u64,
+    pub owners: Vec<String>,
+    pub shares: Vec<u128>,
 }
 
 #[derive(Serialize, Deserialize)]
 pub struct CreateEscrowOutput {
     pub escrow: String,
-    pub operator: String,
-    pub owner: String,
+    pub class_id: u64,
+    pub asset_id: u64,
+    pub owners: Vec<String>,
 }
 
 #[derive(Serialize, Deserialize)]
-pub struct RefundAssetsInput {
+pub struct SweepAssetsInput {
     pub seed: String,
+    pub to: String,
     pub escrow: String,
 }
 
 #[derive(Serialize, Deserialize)]
-pub struct RefundAssetsOutput {
+pub struct SweepAssetsOutput {
     pub escrow: String,
-    pub operator: String,
-    pub owner: String,
+    pub who: String,
+    pub to: String,
 }
 
 #[derive(Serialize, Deserialize)]
 pub struct DepositAssetsInput {
     pub seed: String,
     pub escrow: String,
-    pub class_id: u64,
-    pub asset_ids: Vec<u64>,
-    pub amounts: Vec<u128>,
+    pub class_ids: Vec<u64>,
+    pub asset_ids: Vec<Vec<u64>>,
+    pub amounts: Vec<Vec<u128>>,
 }
 
 #[derive(Serialize, Deserialize)]
 pub struct DepositAssetsOutput {
     pub escrow: String,
-    pub operator: String,
-    pub owner: String,
+    pub who: String,
 }
