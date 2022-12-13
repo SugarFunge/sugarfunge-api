821e357efa73a182a6ecbc1b622c619e8bba7490
carloslopezandara <lopezandara.carlos@gmail.com>
2022-12-13 19:00:00 +0100

    Chasing polkadot 0.9.34

--- src/subscription.rs
+++ src/subscription.rs
@@ -38,29 +38,41 @@ impl SubcriptionServiceWS {
         let tx = tx_origin.clone();
 
         let balances_task = async move {
+            // Subscribe to (in this case, finalized) blocks.
+            let mut block_sub = api.blocks().subscribe_finalized().await?;
+            while let Some(block) = block_sub.next().await {
+                let block = block?;
+        
+                // Ask for the events for this block.
+                let events = block.events().await?;
+        
+                //let block_hash = block.hash();
 
-            let mut balances_events = api.events().subscribe().await.unwrap().filter_events::<(
+                let mut balances_events = events.find::<(
                     sugarfunge::balances::events::Deposit,
                     sugarfunge::balances::events::Transfer,
-            )>();
+                )>();
 
-            while let Some(event) = balances_events.next().await {
-                if let Ok(event) = event {
-                    let event = match event.event {
-                        (None, Some(event)) => serde_json::to_string_pretty(&event),
-                        (Some(event), None) => serde_json::to_string_pretty(&event),
-                        _ => panic!("Invalid event"),
-                    };
-                    if let Ok(event) = event {
-                        tx.send(event).unwrap();
-                    }
-                }
+                /*for event in events.iter() {
+                    let event = event?;
+                    let is_deposit_event = event
+                        .as_event::<sugarfunge::balances::events::Deposit>()?
+                        .is_some();
+                    let pallet = event.pallet_name();
+                    let variant = event.variant_name();
+                    println!(
+                        "    {pallet}::{variant} (is balance transfer? {is_deposit_event})"
+                    );
+                }            */
+
+                
             }
+            
         }
 
         .into_actor(self);
 
-        let api = self.data.api.clone();
+        /*let api = self.data.api.clone();
 
         let tx = tx_origin.clone();
         
@@ -110,19 +122,19 @@ impl SubcriptionServiceWS {
             }
         }
 
-        .into_actor(self);
+        .into_actor(self);*/
 
         let sub: SpawnHandle = ctx.spawn(balances_task);
 
         self.subs.insert("balances_events".into(), sub);
 
-        let sub: SpawnHandle = ctx.spawn(asset_task);
+        /*let sub: SpawnHandle = ctx.spawn(asset_task);
 
         self.subs.insert("asset_events".into(), sub);
 
         let sub: SpawnHandle = ctx.spawn(bag_task);
 
-        self.subs.insert("bag_events".into(), sub);
+        self.subs.insert("bag_events".into(), sub);*/
 
         ctx.run_interval(HEARTBEAT_INTERVAL, move |_act, ctx| {
             if let Ok(event) = rx.try_recv() {
