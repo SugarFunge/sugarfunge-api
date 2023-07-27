b63fc4dbcf3dbc953176c3ecfbc28cccd82e3d3b
MauricinhoMorales <mauricio1999morales@gmail.com>
2023-07-27 16:42:13 -0400

    [Update] Updated the Display of the Module Errors to be more clear

--- src/util.rs
+++ src/util.rs
@@ -30,9 +30,16 @@ pub fn map_sf_err(e: subxt::Error) -> actix_web::Error {
     let subxt::Error::Runtime(DispatchError::Module(module_err)) = e else {
         return error::ErrorBadRequest("Not a Module Error")
     };
-    let value = module_err.as_root_error::<sugarfunge::Error>().unwrap();
 
-    let json_err = json!(&format!("{:?}", value));
+    let mut json_err = json!(&format!("{:?}", value));
+
+    if let Ok(value) = module_err.details() {
+        json_err = json!(&format!(
+            "Pallet: {}, Variant: {}",
+            value.pallet.name(),
+            value.variant.name
+        ));
+    }
 
     let req_error: RequestError = RequestError {
         message: json_err,
