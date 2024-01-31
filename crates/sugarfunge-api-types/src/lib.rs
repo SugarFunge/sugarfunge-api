3a3f27d22d428c328cc23e1ea70e58bb92def6f0
MauricinhoMorales <mauricio1999morales@gmail.com>
2024-01-31 16:22:03 -0500

    Update sugarfunge_metadata.scale

--- crates/sugarfunge-api-types/src/lib.rs
+++ crates/sugarfunge-api-types/src/lib.rs
@@ -1,9 +1,12 @@
 #[subxt::subxt(
     runtime_metadata_path = "sugarfunge_metadata.scale",
-    derive_for_type(path = "frame_support::traits::tokens::misc::BalanceStatus", derive = "serde::Serialize"),
+    derive_for_type(
+        path = "frame_support::traits::tokens::misc::BalanceStatus",
+        derive = "serde::Serialize"
+    ),
     derive_for_type(path = "pallet_balances::pallet::Event", derive = "serde::Serialize"),
     derive_for_type(path = "sugarfunge_asset::pallet::Event", derive = "serde::Serialize"),
-    derive_for_type(path = "sugarfunge_bag::pallet::Event", derive = "serde::Serialize"),
+    derive_for_type(path = "sugarfunge_bag::pallet::Event", derive = "serde::Serialize")
 )]
 pub mod sugarfunge {}
 pub mod account;
